730f3fa7b037b8e89c0427ab9208f8f5a571d1d8
Mahalakshmi <134698247+MahalakshmiPattan@users.noreply.github.com>
2024-02-09 12:10:33 +0530

    Update MediumBrandDesktop.java

--- src/main/java/com/roadrunner/search/dto/MediumBrandDesktop.java
+++ src/main/java/com/roadrunner/search/dto/MediumBrandDesktop.java
@@ -1,13 +1,14 @@
 package com.roadrunner.search.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Data;
 
 @Data
-@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@JsonInclude(value = Include.NON_NULL)
 public class MediumBrandDesktop {
-	
+
 	public MediumBrandFields fields;
 
 }
